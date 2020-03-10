ee972c26c04cce45331c0a2387ab13ea8c922b2f
chumpon-asaneerat <chumponsenate@gmail.com>
2020-03-10 15:04:10 +0700

    Update code for SQLiteSample project. - Create database. - Add record. - Query all rows in table.

--- Samples/SQLiteSample/Form1.cs
+++ Samples/SQLiteSample/Form1.cs
@@ -16,5 +16,27 @@ namespace SQLiteSample
         {
             InitializeComponent();
         }
+
+        private Database db = new Database();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            db.FileName = "sample.db";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            db.Create();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            db.Add(txtUserName.Text, txtPassword.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = db.GetAll();
+        }
     }
 }
