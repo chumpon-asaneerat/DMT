b5e0964bb9c31395aea1eeab0b0056baf6241c6d
chumpon-asaneerat <chumponsenate@gmail.com>
2020-03-17 09:53:26 +0700

    Update 2020-03-17 #1 - Add NLib. - Add PlazaInfo Control.

---
+++ Production/DMT.Controls/Controls/PlazaInfo.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using NLib.Services;
+
+namespace DMT.Controls
+{
+    /// <summary>
+    /// Interaction logic for PlazaInfo.xaml
+    /// </summary>
+    public partial class PlazaInfo : UserControl
+    {
+        public PlazaInfo()
+        {
+            InitializeComponent();
+        }
+
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            PageContentManager.Instance.OnTick += TimerTick;
+        }
+
+        private void UserControl_Unloaded(object sender, RoutedEventArgs e)
+        {
+            PageContentManager.Instance.OnTick -= TimerTick;
+        }
+
+
+        private void UpdateTime()
+        {
+            // update current date time.
+            txtCurrentTime.Text = DateTime.Now.ToString("HH:mm:ss",
+                System.Globalization.DateTimeFormatInfo.InvariantInfo);
+            txtCurrentDate.Text = DateTime.Now.ToString("yyyy-MM-dd",
+                System.Globalization.DateTimeFormatInfo.InvariantInfo);
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            UpdateTime();
+        }
+    }
+}
