d0dd32db0ad15312654954d73bec82026d91bba5
chumpon-asaneerat <chumponsenate@gmail.com>
2020-03-17 13:41:54 +0700

    Update 2020-03-17 #4

---
+++ Production/DMT.Controls/Pages/RevenueEntryPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DMT.Pages
+{
+    /// <summary>
+    /// Interaction logic for RevenueEntryPage.xaml
+    /// </summary>
+    public partial class RevenueEntryPage : UserControl
+    {
+        public RevenueEntryPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
