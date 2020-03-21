c9eebf63b501a2bdfc6ee3013322ab2f78584841
chumpon-asaneerat <chumponsenate@gmail.com>
2020-03-21 22:13:01 +0700

    Update 2020-03-21 #3 - Add DML.Services project - Add PlazaService class.

---
+++ Production/DMT.Services/Services/PlazaService.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMT.Services
+{
+    public class PlazaService
+    {
+        #region Static Instance Access
+
+        private static PlazaService _instance = null;
+
+        public static PlazaService Instance
+        {
+            get
+            {
+                if (null == _instance)
+                {
+                    lock(typeof(PlazaService))
+                    {
+                        _instance = new PlazaService();
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        #endregion
+
+        #region Internal Variables
+
+        #endregion
+
+        #region Constructor and Destructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        private PlazaService()
+        {
+
+        }
+        /// <summary>
+        /// Destructor.
+        /// </summary>
+        ~PlazaService()
+        {
+            Shutdown();
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        /// <summary>
+        /// Start Service.
+        /// </summary>
+        public void Start()
+        {
+
+        }
+        /// <summary>
+        /// Shutdown Service.
+        /// </summary>
+        public void Shutdown()
+        {
+
+        }
+
+        #endregion
+
+        #region Public Events
+
+        #endregion
+    }
+}
