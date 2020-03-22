32c19c6562e9524fbb7b5774f0e12d5c776ef35f
chumpon-asaneerat <chumponsenate@gmail.com>
2020-03-22 18:35:05 +0700

    Update 2020-03-22 #4 - Reload app projects into solution.

---
+++ Production/DMT.Controls/Controls/Compound/DMLPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿#region Using
+
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+using NLib;
+using NLib.Services;
+
+using DMT.Services;
+
+#endregion
+
+namespace DMT.Controls
+{
+    /// <summary>
+    /// Interaction logic for DMLPage.xaml
+    /// </summary>
+    public partial class DMLPage : UserControl
+    {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public DMLPage()
+        {
+            InitializeComponent();
+        }
+
+        #endregion
+    }
+}
