96887b3bbaf4a6e33e68db65dd2a976626e522d0
chumpon-asaneerat <chumponsenate@gmail.com>
2020-04-07 23:36:51 +0700

    Update 2020-04-07 #5 - Add Service Project

---
+++ Prototypes.v2/Code Library/DMT.Prototype.v2.Services/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMT.Prototype.v2.Services
+{
+    public class Class1
+    {
+    }
+}
