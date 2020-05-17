6964b43ec074df62aa02f559dd132ea706a7c346
chumpon-asaneerat <chumponsenate@gmail.com>
2020-05-17 09:03:15 +0700

    Update 2020-05-17 #1 - Update all references required by each projects. - TOD - App.xaml ported. - TOD - MainForm ported. - TA - App.xaml ported. - TA - MainForm ported.

--- Prototypes.v2/App/DMT.TA.Prototype.v2.App/MainWindow.xaml.cs
+++ Prototypes.v2/App/DMT.TA.Prototype.v2.App/MainWindow.xaml.cs
@@ -7,9 +7,30 @@ namespace DMT
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region Constructor
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        #endregion
+
+        #region Load/Unload
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Unloaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        #endregion
     }
 }
